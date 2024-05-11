fd1b3c3af60f70a22aefafb7fb40f08a430357b8
LuizBuarqueDev <luizgabrielbuarque@gmail.com>
2024-05-10 21:33:17 -0300

    Pequenas alterações

---
+++ Analise_de_Projetos_e_Sistemas/src/br/com/ifpe/oficina/apresentation/GUIMenu.java
@@ -0,0 +1,9 @@
+package br.com.ifpe.oficina.apresentation;
+
+public class GUIMenu {
+	
+	public static void runGUI() {
+		
+	}
+
+}
---
+++ Analise_de_Projetos_e_Sistemas/src/br/com/ifpe/oficina/services/dao/CarDAOImpl.java
@@ -0,0 +1,34 @@
+package br.com.ifpe.oficina.services.dao;
+
+import br.com.ifpe.oficina.entities.abstractclasses.Car;
+import br.com.ifpe.oficina.interfaces.IGenericDAO;
+
+public class CarDAOImpl<T> implements IGenericDAO<T>{
+	
+	private Car car;
+
+	@Override
+	public void create(T object) {
+		// TODO Auto-generated method stub
+		this.car = (Car) object;
+	}
+
+	@Override
+	public T read() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(T object) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(T object) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
