e6bfcf4fd02f47762777f2d91c0050a41893767d
Luiz Gabriel Buarque Vasconcelos <lgbv@palmares.ifpe.edu.br>
2024-07-18 15:52:18 -0300

    Modança na forma em que os CPfs são gerados

--- Analise_de_Projetos_e_Sistemas/src/br/com/ifpe/oficina/utils/LoadEntity.java
+++ Analise_de_Projetos_e_Sistemas/src/br/com/ifpe/oficina/utils/LoadEntity.java
@@ -13,20 +13,20 @@ import br.com.ifpe.oficina.services.controllers.CarController;
 import br.com.ifpe.oficina.services.controllers.ClientController;
 
 public class LoadEntity {
-	
-	private static final LoadEntity instance = new LoadEntity();
-	
-	private CarController carController = CarController.getInstance();
-	private ClientController clientController =  ClientController.getInstance();
-	
-	private LoadEntity() {}
+    
+    private static final LoadEntity instance = new LoadEntity();
+    
+    private CarController carController = CarController.getInstance();
+    private ClientController clientController =  ClientController.getInstance();
+    
+    private LoadEntity() {}
 
-	public static LoadEntity getinstance() {
-		return instance;
-	}
-	
-	Random random = new Random();
-	
+    public static LoadEntity getInstance() {
+        return instance;
+    }
+    
+    Random random = new Random();
+    
     public void createRandomData() {
         generateClients(50);
         generateCars(50);
@@ -38,7 +38,7 @@ public class LoadEntity {
         for (int i = 0; i < quantity; i++) {
             String cpf;
             do {
-                cpf = String.format("%011d", 111248547 + i);
+                cpf = generateValidCPF();
             } while (!usedCpfs.add(cpf));
             Client client = Client.ClientBuilder()
                     .age(20 + i)
@@ -92,5 +92,32 @@ public class LoadEntity {
                     System.out.println("Unexpected value: " + key);
             }
         }
+    }
+    
+    private String generateValidCPF() {
+        Random random = new Random();
+        int[] cpfArray = new int[11];
+
+        for (int i = 0; i < 9; i++) {
+            cpfArray[i] = random.nextInt(10);
+        }
+
+        cpfArray[9] = calculateCpfDigit(cpfArray, 9);
+        cpfArray[10] = calculateCpfDigit(cpfArray, 10);
+
+        StringBuilder cpf = new StringBuilder();
+        for (int digit : cpfArray) {
+            cpf.append(digit);
+        }
+        return cpf.toString();
+    }
+
+    private int calculateCpfDigit(int[] cpf, int length) {
+        int sum = 0;
+        for (int i = 0; i < length; i++) {
+            sum += cpf[i] * ((length + 1) - i);
+        }
+        int result = sum % 11;
+        return (result < 2) ? 0 : 11 - result;
     }
 }
